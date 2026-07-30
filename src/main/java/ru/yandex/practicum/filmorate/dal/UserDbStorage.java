64833078dae75591b22fb8395880655307d82bec
Dmitrii Konstantinov <dimonkent61@yandex.ru>
2026-07-30 23:45:25 +0300

    remove unused import

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.dal;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
