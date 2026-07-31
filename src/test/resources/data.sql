98662c15cd18504ca249039547c708ded0b099f4
Dmitrii Konstantinov <dimonkent61@yandex.ru>
2026-07-31 11:05:30 +0300

    fix data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO mpa_ratings (id, name) VALUES (1, 'G');
-INSERT INTO mpa_ratings (id, name) VALUES (2, 'PG');
-INSERT INTO mpa_ratings (id, name) VALUES (3, 'PG-13');
-INSERT INTO mpa_ratings (id, name) VALUES (4, 'R');
-INSERT INTO mpa_ratings (id, name) VALUES (5, 'NC-17');
+MERGE INTO mpa_ratings (id, name) VALUES (1, 'G');
+MERGE INTO mpa_ratings (id, name) VALUES (2, 'PG');
+MERGE INTO mpa_ratings (id, name) VALUES (3, 'PG-13');
+MERGE INTO mpa_ratings (id, name) VALUES (4, 'R');
+MERGE INTO mpa_ratings (id, name) VALUES (5, 'NC-17');
 
-INSERT INTO genres (id, name) VALUES (1, 'Комедия');
-INSERT INTO genres (id, name) VALUES (2, 'Драма');
-INSERT INTO genres (id, name) VALUES (3, 'Мультфильм');
-INSERT INTO genres (id, name) VALUES (4, 'Триллер');
-INSERT INTO genres (id, name) VALUES (5, 'Документальный');
-INSERT INTO genres (id, name) VALUES (6, 'Боевик');
+MERGE INTO genres (id, name) VALUES (1, 'Комедия');
+MERGE INTO genres (id, name) VALUES (2, 'Драма');
+MERGE INTO genres (id, name) VALUES (3, 'Мультфильм');
+MERGE INTO genres (id, name) VALUES (4, 'Триллер');
+MERGE INTO genres (id, name) VALUES (5, 'Документальный');
+MERGE INTO genres (id, name) VALUES (6, 'Боевик');
